5eed1952ca1aa75a2e378ec7d01159f3a9f341ce
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2020-11-29 20:55:37 -0800

    Remove a bunch of stuff from comment block in main, add more params

--- src/clj_vectors_perf/main.clj
+++ src/clj_vectors_perf/main.clj
@@ -14,12 +14,19 @@
 (defn =-vec-and-vec-ret-true [n]
   (let [v1 (vec (range n))
         v2 (vec (range n))]
+    ;; It is important when using Criterium to measure the performance
+    ;; of (= v1 v2) that the values of v1 and v2 have been initialized
+    ;; outside of the expression given to crit/benchmark, otherwise
+    ;; their construction will be included in the measurement.
     (crit/with-progress-reporting
       (crit/benchmark (= v1 v2) crit-opts))))
 
 (defn perf-=-vec-and-vec-ret-true []
   (sweep-params =-vec-and-vec-ret-true
-                [[1] [2]]))
+                [[1] [2] [4] [8] [16] [32] [64]
+                 [128] [256] [512] [1024] [2048]
+                 [4096] [8192] [16384] [32768] [65536]
+                 [100000]]))
 
 (comment
     
@@ -57,50 +64,5 @@ v1
         v2 (assoc v2 (dec n) 0)]
     (frequencies (mapv (fn [_] (time (= v1 v2)))
                        (range ntimes)))))
-
-(defn hash-vec-size-n [ntimes n]
-  (let [vecs (mapv (fn [_] (vec (range n))) (range ntimes))]
-    (frequencies (mapv (fn [v] (time (hash v)))
-                       vecs))))
-
-(compare-vec-size-n 50 50)
-(compare-vec-size-n 1000)
-(compare-vec-size-n 10000)
-(compare-vec-size-n 50 1000000)
-(compare-vec-size-n-neq 50 1000000)
-
-(compare-vec-to-seq-size-n 1 20)
-
-(hash-vec-size-n 50 1000000)
-
-(println "first of 10")
-
-;; With Clojure 1.10.1 modified with my changes to
-;; APersistentVector.doEquiv that uses List iterator() method instead
-;; of nth(), times ranged from:
-
-"Elapsed time: 13.021462 msecs"
-"Elapsed time: 13.322932 msecs"
-"Elapsed time: 14.192369 msecs"
-"Elapsed time: 15.139772 msecs"
-"Elapsed time: 15.697675 msecs"
-"Elapsed time: 15.830441 msecs"
-"Elapsed time: 17.473316 msecs"
-"Elapsed time: 19.406707 msecs"
-"Elapsed time: 19.925345 msecs"
-"Elapsed time: 19.994562 msecs"
-
-;; with unmodified Clojure 1.10.1, times ranged from:
-
-"Elapsed time: 20.749841 msecs"
-"Elapsed time: 20.993374 msecs"
-"Elapsed time: 21.082402 msecs"
-"Elapsed time: 21.124058 msecs"
-"Elapsed time: 21.707172 msecs"
-"Elapsed time: 23.086769 msecs"
-"Elapsed time: 23.955181 msecs"
-"Elapsed time: 27.352486 msecs"
-"Elapsed time: 34.673524 msecs"
-"Elapsed time: 37.507745 msecs"
 
 )
